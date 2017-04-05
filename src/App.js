22cd7798a85e629540b96de2dc06be517dbc5cd8
Jay Kid <cyph3rfox@gmail.com>
2017-04-05 19:00:39 +0200

    cleaned up a bit
    + Removed create-react-app logo, App styles and updated a lil' bit ( it's uglier now :'( ) the styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import Preview from './components/preview';
 import Editor from './components/editor';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Markdown Editor</h2>
-        </div>
+        <header>
+          <h1>Markdown Editor</h1>
+        </header>
         <main>
           <Editor />
           <Preview />
